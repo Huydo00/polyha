48d917eca6086d2289ddb739d92a7d7d3acd535c
Huy N Do <hhuys0105@gmail.com>
2024-04-25 22:03:19 +0700

    123

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOgitR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3,5)
     
     for (x,y,w,h) in faces:
