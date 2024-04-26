ca60e43dba230347fed81691edd58269c2ac0ff1
Huy N Do <hhuys0105@gmail.com>
2024-04-26 08:50:39 +0700

    1

---
+++ final.py
@@ -0,0 +1,9 @@
+
+
+frame_count = {'blink':39, 'happy':60, 'sad':47,'dizzy':67,'excited':24,'neutral':61,'happy2':20,'angry':20,'happy3':26,'bootup3':124,'blink2':20}
+
+emotion = ['angry','sad','excited']
+
+normal = ['neutral','blink2']
+
+multiprocessing.Process(target=happy)
